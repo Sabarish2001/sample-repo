6ad7c55867aeff89cc0a8a7b100888463c6cf512
Sabarish2001 <sabarishb2001@gmail.com>
2023-02-28 22:06:10 +0530

    modified the variables and list names in cal_pos.py file

--- cal_pos.py
+++ cal_pos.py
@@ -1,11 +1,11 @@
 def cal_pos(Genome,pattern):
-	index_list = []
-	t = len(Genome)
+	pos_list = []
+	g = len(Genome)
 	p = len(pattern)
 	
-	for every_letter in range(t - p  + 1 ):
+	for n  in range(g - p  + 1 ):
 		if Genome[i : i + p] == pattern:
-			index_list.append(i)
-	return index_list
+			pos_list.append(i)
+	return pos_list
 	
 
